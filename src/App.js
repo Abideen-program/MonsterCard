ee30272eb1510c7425d7b392bcaf35a3b5475ac9
Abideen Olafimihan <olafimihana@gmail.com>
2022-06-15 21:38:11 +0100

    updating App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,38 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-    
+    constructor() {
+      super()
+
+      this.state = {
+       monsters: [
+        {
+          name: 'Jackie',
+          color: 'Black'
+        },
+
+        {
+          name: 'Babel',
+          color: 'Green'
+        },
+
+        {
+          name: 'Agott',
+          color: 'Blue'
+        }
+
+       ] 
+      }
+    }
     render() {
       return (
-        <div className="App"></div>
+        <div className="App">
+            {
+              this.state.monsters.map( (monster) => {
+                  return <h2>My name is {monster.name}, and I am {monster.color}</h2>
+              } )
+            }
+        </div>
       );
     }
 }
